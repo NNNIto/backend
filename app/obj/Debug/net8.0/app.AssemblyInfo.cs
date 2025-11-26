09023c26e7d464bce8409c9dfd4d18b8e5b0748b
NNNIto <ito.natsuki.922@gmail.com>
2025-11-26 23:57:41 +0000

    commit

--- app/obj/Debug/net8.0/app.AssemblyInfo.cs
+++ app/obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4a6b27643f336ac62066ba67ce60133016fc8f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70b5e1990198d8d5b63fb46f6a0b2b88649f5205")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
